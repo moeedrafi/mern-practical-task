23a11b6603ec9aa15d1f3cf182f913daf23e0435
Moeed Rafi <moeedrafi95@gmail.com>
2025-04-23 22:48:12 +0500

    setup: Express API

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,8 @@
 import cors from "cors";
 import express from "express";
 import cookieParser from "cookie-parser";
+import productsRouter from "./routes/products.routes.js";
+import { errorHandler } from "./middleware/errorHandler.middleware.js";
 
 const app = express();
 app.use(
@@ -12,7 +14,10 @@ app.use(
 
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
-app.use(express.static("public"));
 app.use(cookieParser());
+
+app.use("/api/v1/products", productsRouter);
+
+app.use(errorHandler);
 
 export { app };
